4d671fe096ddd7ccf685fe7b3cb2b3689e170d64
mcdiaz <mcarolinadiaz0@gmail.com>
2019-09-19 16:19:49 -0300

    Matriz de transicion

--- src/logic/Lexicon.java
+++ src/logic/Lexicon.java
@@ -3,7 +3,9 @@ package logic;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Queue;
 
 import utils.MsgStack;
@@ -13,19 +15,23 @@ import utils.Token;
 public class Lexicon {
 	private final int END_OF_FILE = -1;
 	private int filePtr;
-	private Queue<Integer> programBuffer;
+	private List<Integer> programBuffer;
 	private int nlCounter;
 	private MsgStack msgStack;
-	private Hashtable<Character, Integer> stateMachine;
+	//private Hashtable<Integer, Hashtable<Character, Integer>> stateMachine;
+	private Integer[][] stateMachine;
 	private Hashtable<Character, SemanticAction> semanticAction;
+	private Hashtable<Integer, String> simbTable;
 	
 	
 	
 	public Lexicon(String srcCode, MsgStack msgStack) {
 		this.nlCounter = 0;
 		this.msgStack = msgStack;
+		this.programBuffer=new ArrayList<>();
 		openFile(srcCode);
 		filePtr = 0;
+		this.stateMachine=new Integer[8][13];
 		initMachineState();
 		initSemanticActions();
 	}
@@ -35,7 +41,11 @@ public class Lexicon {
 	public Token getNewToken() {
 		String lexeme; //El lexema y el conteo de nl debe ir en las AS (¿cómo ponerlo ahí?)
 		//TODO: Itera en la máquina de estados hasta tener un token
-		
+		int state=0;
+		while(!this.programBuffer.isEmpty())
+		{
+			//char charac=(char)this.programBuffer.get(0);
+		}
 			//TODO: Leer caracter, buscar en tabla de transiciones
 		
 			//TODO: Aplicar AS si existe
@@ -68,6 +78,31 @@ public class Lexicon {
 	
 	private void initMachineState() {
 		//TODO: Inicializar con la matriz adecuada
+		/*for(int state=0;state<8;state++)
+		{
+			this.stateMachine.put(state, new Hashtable<Character,Integer>());
+		}
+		this.stateMachine.get(0).put('L', 1);
+		this.stateMachine.get(0).put('D', 2);
+		this.stateMachine.get(0).put('_', 0);
+		this.stateMachine.get(0).put('$', -1);
+		this.stateMachine.get(0).put('SE', -1);
+		this.stateMachine.get(0).put('{', 3);
+		this.stateMachine.get(0).put('}', 0);
+		this.stateMachine.get(0).put('<', 5);
+		this.stateMachine.get(0).put('>', 2);
+		this.stateMachine.get(0).put('=', 2);
+		this.stateMachine.get(0).put('nl', 2);
+		this.stateMachine.get(0).put('#', 2);
+		this.stateMachine.get(0).put('TAB ', 2);
+		this.stateMachine.get(0).put('otros', 2);
+		*/
+		
+		///Hecho por matriz[estado][simb]
+		this.stateMachine[0][0]=1;
+		
+		
+		
 	}
 	
 	private void initSemanticActions () {
